97ee75810f9d6b9c8fcad0dec99d119da462aae3
Salman Shah <smsalmanshah@gmail.com>
2024-08-21 17:14:49 +0500

    feat: made hero section responsive

--- src/components/Hero.vue
+++ src/components/Hero.vue
@@ -2,13 +2,14 @@
 </script>
 
 <template>
-    <div class="bg-green-950 text-center flex flex-col p-8">
+    <div class="  bg-green-950 text-center flex flex-col  py-16 justify-center items-center content-center
+    sm:flex-row-reverse">
 
-        <div>
-            <h1 class="font-title font-bold text-2xl text-amber-400">AlKahaf Charitable Trust</h1>
-            <h2 class="font-body font-medium text-lg text-green-50">Bringing the community together with a focus on education, self development & spirituality</h2>
+        <div class=" w-3/5 ">
+            <h1 class="font-title  text-amber-400 font-bold text-2xl md:text-4xl">AlKahaf Charitable Trust</h1>
+            <h2 class="font-body font-medium text-lg text-green-50 md:text-2xl">Bringing the community together with a focus on education, self development & spirituality</h2>
         </div>
-        <img src="/Logo Vertical.svg" alt="" srcset="" class="h-48">
+        <img src="/Logo Vertical.svg" alt="" srcset="" class="h-48 sm:p-8 sm:h-64  md:h-80 ">
     </div>
 
 </template>
