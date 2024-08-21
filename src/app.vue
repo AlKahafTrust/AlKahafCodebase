55e13abaccb263ebb085a6de4f346659937db8ba
Salman Shah <smsalmanshah@gmail.com>
2024-08-21 18:47:54 +0500

    feat:added coming soon section

--- src/app.vue
+++ src/app.vue
@@ -1,11 +1,12 @@
 <script setup>
     import Hero from './components/Hero.vue'
+    import ComingSoon from './components/ComingSoon.vue'
 </script>
 
 <template>
 
     <Hero/>
+    <ComingSoon/>
 <!-- 
-<ComingSoon>
  -->
 </template>
---
+++ src/components/ComingSoon.vue
@@ -0,0 +1,22 @@
+<script setup>
+</script>
+
+<template>
+    <div class="font-body text-center text-lg bg-green-50 py-16">
+        <h2  class="font-title  text-green-950 font-bold text-2xl md:text-4xl">Website Coming Soon</h2>
+        <p class="text-green-950 md:text-2xl "> Meanwhile you can reach out to us for any information</p>
+    
+        <div class="flex flex-col justify-center align-start sm:flex-row ">
+            <div class="flex flex-col p-8">
+                <img src="/icons/iconWhatsapp.svg" alt="" srcset="" class="h-12 pb-4 ">
+                <a href="https://wa.me/9198664799431" target="_blank"><p> +91 986 6479 9431</p></a>
+                <a href="https://wa.me/918341642632" target="_blank"><p>+91 834 164 2632</p></a>
+                <a href="https://wa.me/966507781084" target="_blank"><p>+966 50 778 1084</p></a>
+            </div>
+            <div class="flex flex-col p-8">
+                <img src="/icons/iconGmail.svg" alt="" srcset="" class="h-12 pb-4">
+                <a href="mailto:contact@alkahaftrust.com"><p> contact@alkahaftrust.com</p></a>
+            </div>
+        </div>
+    </div>
+</template>
